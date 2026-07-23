18c80e26a108ade225ca96cabee5616621b955d4
Satyanarayan Behera <satyanarayanbehera075@gmail.com>
2026-07-23 23:53:58 +0530

    Adding Bitwise Operations file

---
+++ bitwise_operator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+//     cout<<(6 & 10)<<endl;
+//     cout<<(6 | 10)<<endl;
+//     cout<<(6 ^ 10)<<endl;
+    
+//     cout<<(10<<2)<<endl;
+//     cout<<(10>>1)<<endl;
+
+
+//     return 0;
+// }
+
+//----------------->
+//Checking a number whether it is power of '2' or not
+// int main(){   
+//     int n;
+//     cout<<"Checking a number whether it is power of '2' or not"; 
+//     cout<<"\nEnter a number : "; cin>>n;
+//     if(n>0 && (n&(n-1))==0){      //Bitwise And(&) operator
+//         cout<<"True";
+//     }else{
+//         cout<<"false";
+//     }
+
+// return 0;
+// }
+
+//------------------>
+//reverse an input integers
+// int main(){
+//     int n , ans=0;
+//     cout<<"Enter a number : "; cin>>n;
+//     while(n > 0){
+//         int rem = n % 10;
+//         n/=10;
+//         ans = (ans*10) + rem;
+//     }
+//     cout<<"Reversed number is : " << ans<<endl;
+
+//     return 0;
+// }
+
+
